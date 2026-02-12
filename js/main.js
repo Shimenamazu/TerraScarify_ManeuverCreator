15d3654655b69e079881f344c4805b9d5510c83f
Shimenamazu <shimenamazu.shime.shimena@gmail.com>
2026-02-12 19:23:44 +0900

    トークン計算

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
  ********************************/
 
 //htmlタブ
+const debug = document.getElementById("debug");
+const type_select = document.getElementById("type-select"); //タイプ選択
+const tmg_select = document.getElementById("tmg-select"); //タイミング選択
+const pow_select = document.getElementById("pow-select"); //タイミング選択
 const add_eff_button = document.getElementById("add-eff-button"); //エフェクト追加ボタン
 const add_eff_button_text = document.getElementById("add-eff-button-text"); //エフェクト追加ボタンのテキスト
 const add_eff_panel = document.getElementById("add-eff-panel"); //エフェクト追加パネル
@@ -13,6 +17,19 @@ const power_select = document.getElementById("power-select"); //効果選択プ�
 
 //変数
 let unit_target = "ユニット1体"; //ユニット対象の数
+let t_token = [0, 0, 0]; //タイミングトークン
+let r_token = [0, 1, 3]; //判定トークン
+let p_token = [0, 0, 0]; //ペナルティトークン
+let gc_token = [0, 0, 0]; //補正トークン(獲得)
+let g_token = [0, 0, 0]; //獲得トークン
+let e_token = [0, 0, 0]; //エフェクトトーク
+let a_token = [0, 0, 0]; //アライブトークン
+let cc_token = [0, 0, 0]; //補正トークン(消費)
+let c_token = [0, 0, 0]; //消費トークン
+let l_token = [0, 0, 0]; //不足トークン
+let man_type = "man";
+let man_tmg = "adj";
+let man_pow = "full";
 
 //クラス
 class Effect {
@@ -138,9 +155,9 @@ for (const key in eff_list) {
   eff_list_box.appendChild(btn);
 }
 
-/********************************
-  各ボタンクリック時の挙動
- ********************************/
+/*********************************
+    関数
+ *********************************/
 //エフェクト選択リストの表示
 function show_eff_list() {
   build_eff_editer.classList.add("hidden"); //エディタを非表示
@@ -154,6 +171,102 @@ function show_eff_list() {
   target_select.options.length = 0;
   power_select.options.length = 0;
 }
+
+const tkn_label = ["f", "s", "c"]; //id特定用
+
+//トークン計算
+function cal_get_token() {
+  if (man_type == "man") {
+    t_token = [0, 0, 0];
+    if (man_tmg == "main") {
+      r_token = [0, 2, 4];
+    } else {
+      r_token = [0, 1, 3];
+    }
+    if (man_pow == "half") {
+      r_token[1] += 1;
+    }
+  } else {
+    r_token = [0, 1, 3];
+    if (man_tmg == "main") {
+      t_token = [2, 2, 2];
+    } else {
+      t_token = [1, 1, 1];
+    }
+    if (man_pow == "half") {
+      r_token[1] += 1;
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    g_token[i] = t_token[i] + r_token[i] + p_token[i] + gc_token[i];
+    document.getElementById("t-t-" + tkn_label[i]).textContent = t_token[i];
+    document.getElementById("t-r-" + tkn_label[i]).textContent = r_token[i];
+    document.getElementById("t-p-" + tkn_label[i]).textContent = p_token[i];
+    document.getElementById("t-gc-" + tkn_label[i]).textContent = gc_token[i];
+    document.getElementById("t-g-" + tkn_label[i]).textContent = g_token[i];
+    document.getElementById("t-g2-" + tkn_label[i]).textContent = g_token[i];
+  }
+}
+
+function cal_com_token() {
+  for (let i = 0; i < 3; i++) {
+    c_token[i] = e_token[i] + a_token[i] + cc_token[i];
+    document.getElementById("t-e-" + tkn_label[i]).textContent = e_token[i];
+    document.getElementById("t-a-" + tkn_label[i]).textContent = a_token[i];
+    document.getElementById("t-cc-" + tkn_label[i]).textContent = cc_token[i];
+    document.getElementById("t-c-" + tkn_label[i]).textContent = c_token[i];
+    document.getElementById("t-c2-" + tkn_label[i]).textContent = c_token[i];
+  }
+}
+
+function cal_lack_token() {
+  cal_get_token();
+  cal_com_token();
+  for (let i = 0; i < 3; i++) {
+    l_token[i] = c_token[i] - g_token[i];
+    document.getElementById("t-l-" + tkn_label[i]).textContent = l_token[i];
+  }
+}
+
+//バースト・アライブ表示
+function show_BA() {
+  document.querySelectorAll(".only-BA").forEach((panel) => {
+    panel.classList.remove("hidden");
+  });
+}
+
+function hidden_BA() {
+  document.querySelectorAll(".only-BA").forEach((panel) => {
+    panel.classList.add("hidden");
+  });
+}
+/********************************
+  各ボタンクリック時の挙動
+ ********************************/
+//タイプ選択
+type_select.addEventListener("change", () => {
+  man_type = type_select.value;
+  if (man_type == "man") {
+    hidden_BA();
+  } else if (man_type == "burst") {
+    show_BA();
+  } else if (man_type == "alive") {
+    show_BA();
+  }
+  cal_lack_token();
+});
+
+//タイミング選択
+tmg_select.addEventListener("change", () => {
+  man_tmg = tmg_select.value;
+  cal_lack_token();
+});
+
+//倍率選択
+pow_select.addEventListener("change", () => {
+  man_pow = pow_select.value;
+  cal_lack_token();
+});
 
 //エフェクト追加ボタン
 add_eff_button.addEventListener("click", () => {
@@ -194,3 +307,16 @@ document.getElementById("build-eff-rechoice").addEventListener("click", () => {
   //
   show_eff_list();
 });
+
+/********************************
+  デバッグ
+ ********************************/
+debug.addEventListener("click", () => {
+  cal_lack_token();
+});
+
+/********************************
+  セットアップ
+ ********************************/
+hidden_BA();
+cal_lack_token();
